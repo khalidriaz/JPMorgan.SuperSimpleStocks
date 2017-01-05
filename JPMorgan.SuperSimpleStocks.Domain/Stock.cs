13c26011657b9fb1d53d8c90badb840a20351671
Khalid Riaz <khalidriaz26@hotmail.com>
2017-01-05 13:22:31 +0000

    Method for Common and Preferred stock dividend yield and unit tests. Implementation of the IStock interface.

--- JPMorgan.SuperSimpleStocks.Domain/Stock.cs
+++ JPMorgan.SuperSimpleStocks.Domain/Stock.cs
@@ -6,31 +6,47 @@ using System.Threading.Tasks;
 
 namespace JPMorgan.SuperSimpleStocks.Domain
 {
-    public class Stock
+    public class Stock: IStock
     {
         readonly double _lastDividend;
         readonly double _fixedDividend;
-
+        readonly StockType _stockType;
         public double ParValue { get; private set; }// price in pennies
         public string StockSymbol { get; private set; }
 
         public Stock(string symbol, double parValue,
-            double lastDividend, double fixedDividend = 0)
+            double lastDividend, StockType stockType, double fixedDividend = 0)
         {
             StockSymbol = symbol;
             ParValue = parValue;
 
             _lastDividend = lastDividend;
             _fixedDividend = fixedDividend;
+            _stockType = stockType;
         }
 
-        public double GetDividendYeild(string stockSymbol, double marketPrice)
+        public double DividendYield(double marketPrice)
         {
             if (marketPrice == 0)
                 throw new DivideByZeroException();
 
-            double dividenYield = _lastDividend / marketPrice;
+            double dividenYield = 0;
+
+            if (_stockType == StockType.Common)
+            {
+                dividenYield = _lastDividend / marketPrice;
+            }
+            else if (_stockType == StockType.Preferred)
+            {
+                dividenYield = ((_fixedDividend / 100) * ParValue) / marketPrice;
+            }
+            
             return dividenYield;
+        }
+
+        public double PERatio(double marketPrice)
+        {
+            throw new NotImplementedException();
         }
     }
 }
--- JPMorgan.SuperSimpleStocks.Test/StockTests.cs
+++ JPMorgan.SuperSimpleStocks.Test/StockTests.cs
@@ -11,21 +11,34 @@ namespace JPMorgan.SuperSimpleStocks.Test
         [Test]
         public void Stocks_Object_Is_Not_Null()
         {
-            var stock = new Stock("POP", 100, 8);
+            var stock = new Stock("POP", 100, 8, StockType.Common);
             stock.Should().NotBeNull();
         }
 
         [Test]
-        public void Calculate_Dividend_Yield_Test_Returns()
+        public void Calculate_Common_Stock_Dividend_Yield_Test()
         {
             //Arrange
-            var stock = new Stock("POP", 100, 8);
+            var stock = new Stock("POP", 100, 8, StockType.Common);
 
             //Act
-            double dividend = stock.GetDividendYeild("POP", 10);
+            double dividend = stock.DividendYield(10);
 
             //Assert
             dividend.Should().BeApproximately(0.8, 0.01);
+        }
+
+        [Test]
+        public void Calculate_Preferred_Stock_Dividend_Yield_Test()
+        {
+            //Arrange
+            var stock = new Stock("GIN", 100, 8, StockType.Preferred, 2);
+
+            //Act
+            double dividend = stock.DividendYield(10);
+
+            //Assert
+            dividend.Should().BeApproximately(0.2, 0.01);
         }
     }
 }
