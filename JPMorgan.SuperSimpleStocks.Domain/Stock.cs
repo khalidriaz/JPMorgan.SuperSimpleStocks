319ea157bbf0deb2070c5f67d3e562256874750d
Khalid Riaz <khalidriaz26@hotmail.com>
2017-01-05 13:07:23 +0000

    GetDividendYield method unit test and implementation.

--- JPMorgan.SuperSimpleStocks.Domain/Stock.cs
+++ JPMorgan.SuperSimpleStocks.Domain/Stock.cs
@@ -8,9 +8,29 @@ namespace JPMorgan.SuperSimpleStocks.Domain
 {
     public class Stock
     {
+        readonly double _lastDividend;
+        readonly double _fixedDividend;
+
+        public double ParValue { get; private set; }// price in pennies
+        public string StockSymbol { get; private set; }
+
+        public Stock(string symbol, double parValue,
+            double lastDividend, double fixedDividend = 0)
+        {
+            StockSymbol = symbol;
+            ParValue = parValue;
+
+            _lastDividend = lastDividend;
+            _fixedDividend = fixedDividend;
+        }
+
         public double GetDividendYeild(string stockSymbol, double marketPrice)
         {
-            throw new NotImplementedException();
+            if (marketPrice == 0)
+                throw new DivideByZeroException();
+
+            double dividenYield = _lastDividend / marketPrice;
+            return dividenYield;
         }
     }
 }
--- JPMorgan.SuperSimpleStocks.Test/StockTests.cs
+++ JPMorgan.SuperSimpleStocks.Test/StockTests.cs
@@ -9,12 +9,22 @@ namespace JPMorgan.SuperSimpleStocks.Test
     public class StockTests
     {
         [Test]
+        public void Stocks_Object_Is_Not_Null()
+        {
+            var stock = new Stock("POP", 100, 8);
+            stock.Should().NotBeNull();
+        }
+
+        [Test]
         public void Calculate_Dividend_Yield_Test_Returns()
         {
-            var stock = new Stock();
-            string stockSymbol = "POP";
-            double marketPrice = 10;
-            double dividend = stock.GetDividendYeild(stockSymbol, marketPrice);
+            //Arrange
+            var stock = new Stock("POP", 100, 8);
+
+            //Act
+            double dividend = stock.GetDividendYeild("POP", 10);
+
+            //Assert
             dividend.Should().BeApproximately(0.8, 0.01);
         }
     }
